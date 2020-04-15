896a94237174b63d88d3057f2749a5e1daa714d8
Matthew Purver <m.purver@qmul.ac.uk>
2020-04-15 16:41:26 +0200

    dockerize model download

--- services/web/project/models/model_download.sh
+++ services/web/project/models/model_download.sh
@@ -1,2 +1,3 @@
-wget -O ./services/web/project/models/ml_hate_speech_classifier.zip https://www.dropbox.com/s/yd9t2wihv0x0qxq/ml_hate_speech_classifier.zip && unzip ./services/web/project/models/ml_hate_speech_classifier.zip -d ./services/web/project/models/
+wget -O ./project/models/ml_hate_speech_classifier.zip https://www.dropbox.com/s/yd9t2wihv0x0qxq/ml_hate_speech_classifier.zip
+unzip ./project/models/ml_hate_speech_classifier.zip -d ./project/models/
 
