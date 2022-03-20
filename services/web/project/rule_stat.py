39a78a883cf8657933f4a1482aa0fcf61a875b0e
Ravi Shekhar <shekharRavi@users.noreply.github.com>
2022-03-20 11:45:56 +0000

    r

--- services/web/project/rule_stat.py
+++ services/web/project/rule_stat.py
@@ -5,7 +5,7 @@ file_name = '/import/cogsci/ravi/datasets/Embeddia/STY_24sata_comments_hr_001.cs
 
 df = pd.read_csv(file_name, parse_dates=['created_date','last_change'],low_memory=False)
 df = df.fillna(0)
-
+print('Reading Done')
 rules = [0,1,2,3,4,5,6,7,8]
 
 data ={}
@@ -15,5 +15,5 @@ for rule in rules:
 
     data[rule]=len(sel_data)
 
-
+print(data)
 df.to_csv('rule_stat.csv', index=False)
