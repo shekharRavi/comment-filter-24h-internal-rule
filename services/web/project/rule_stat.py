c0d5b2f47aea4edf1a5d09525dabafdaf81edb82
Ravi Shekhar <shekharRavi@users.noreply.github.com>
2022-03-20 11:44:12 +0000

    d

---
+++ services/web/project/rule_stat.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+file_name = '/import/cogsci/ravi/datasets/Embeddia/STY_24sata_comments_hr_001.csv'
+
+df = pd.read_csv(file_name, parse_dates=['created_date','last_change'],low_memory=False)
+df = df.fillna(0)
+
+rules = [0,1,2,3,4,5,6,7,8]
+
+data ={}
+for rule in rules:
+    sel_data = df[df.infringed_on_rule == rule]
+    print(rule, len(sel_data))
+
+    data[rule]=len(sel_data)
+
+
+df.to_csv('rule_stat.csv', index=False)
