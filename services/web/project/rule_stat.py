caf0062522bc4005066f3c4c33385ed3c41f43e3
Ravi Shekhar <shekharRavi@users.noreply.github.com>
2022-03-20 12:15:42 +0000

    d

--- services/web/project/rule_stat.py
+++ services/web/project/rule_stat.py
@@ -16,4 +16,28 @@ for rule in rules:
     data[rule]=len(sel_data)
 
 print(data)
-df.to_csv('rule_stat.csv', index=False)
+data.to_csv('rule_stat.csv', index=False)
+
+rules = [2,3,4,5,6,7,1,8,0]
+
+data ={}
+for idx, rule in enumerate(rules):
+    sel_data = df[df.infringed_on_rule == rule]
+    # print('s',rule, len(sel_data), len(save_df))
+    if idx == 0:
+        save_df = sel_data
+    else:
+        if rule in [8,0,1]:
+            if len(save_df) < len(sel_data):
+                n = int(len(save_df) * 1.5)
+                if n < len(sel_data):
+                    # print('n.',n, 'sel',len(sel_data))
+                    sel_data = sel_data.sample(n = n)
+        save_df = pd.concat([save_df, sel_data])
+    
+    data[rule] = len(sel_data)
+
+    # print(rule, len(sel_data), len(save_df))
+print(data)
+data.to_csv('rule_stat_selected.csv', index=False)
+save_df.to_csv('rule_selected_24sata.csv', index=False)
