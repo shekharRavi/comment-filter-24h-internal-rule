6476c6d18f3f9e378f9d3611d82ba7a6671519fc
Ravi Shekhar <shekharRavi@users.noreply.github.com>
2022-03-21 11:02:27 +0000

    d

--- services/web/project/train_code.py
+++ services/web/project/train_code.py
@@ -97,7 +97,7 @@ class CustomTrainer(Trainer):
         outputs = model(**inputs)
         logits = outputs.get("logits")
         # compute custom loss (suppose one has 3 labels with different weights)
-        loss_fct = nn.CrossEntropyLoss(weight=self.class_weights)
+        loss_fct = torch.nn.    (weight=self.class_weights)
         loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
         return (loss, outputs) if return_outputs else loss
 
