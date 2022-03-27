652c9f9114116deb2132d29263eb85239a87dc73
Ravi Shekhar <shekharRavi@users.noreply.github.com>
2022-03-27 11:40:29 +0100

    d

--- services/web/project/test_save_output.py
+++ services/web/project/test_save_output.py
@@ -15,6 +15,190 @@ logger = logging.getLogger(__name__)
 from datasets import load_metric
 from datasets import load_from_disk
 
+import re
+from lingua import Language, LanguageDetectorBuilder
+
+from langdetect import detect
+
+lang_detector = LanguageDetectorBuilder.from_all_languages().with_preloaded_language_models().build()
+valid_langs= ['BOSNIAN', 'SERBIAN','CROATIAN','MONTENEGRIN','ENGLISH', 'SLOVENE','SLOVAK', 'hr','sr','bs','cnr','en','sl','slv']
+
+not_allowed_words = ['24sata', 'admin', 'Adolf', 'Hitler', 'jebem', 'jebemti', 'pička', 'lezba', 'majmun', 
+                     'mater', 'majku', 'kurac', 'seri', 'seres', 'nabijem', 'glup', 'idijot', 'idiot', 'budala', 
+                     'kreten', 'pizda', 'klad', 'kladi', 'ZDS', 'klaunski', 'mater', 'smece', 'tenkre', 'bilde',
+                     'debil', 'jebi', 'cigan', 'govn', 'seljac', 'drolj', 'kozojeb', 'musliman', 'klaunski', 
+                     'derpe', 'maloumni', 'hebe', 'racku', 'spodob', 'kita', 'stoka', 'crkn', 'debel', 'krmaca',
+                     'dubre', 'djubre', 'retard', 'barbika', 'miss piggy', 'srb', 'kme', 'novina', 'pick', 
+                     'nepism', 'sipt', 'ptar', 'za dom', 'srps', 'tuka', 'jeb, peni', 'udba, čas', 'šiptar',
+                     'šupak', 'ustaša', 'smrad', 'budal', 'kopile', 'imbecil', 'guba', 'stoko', 'icka', 'ubre',
+                     'gnjida', 'ljubičice', 'štraca', 'šljam', 'pupavac', 'jado', 'bilde', 'straca', 'sereš', 
+                     'đubre', 'čedo', 'pička', 'jadnič', 'četn', 'kurc', 'krmača', 'lomač', 'metak', 'čelo', 
+                     'jebo', 'ubit', 'asenovac', 'cetni', 'gamad', 'kurva', 'peder', 'kurvetina', 'kurv', 
+                     'kobilo', 'degenerik', 'panglu', 'tenkre', 'smeće', 'sme', 'smece', 'sponzoruša', 'sponzorusa', 
+                     'sponz', 'konju', 'krivousti', 'krivou', 'hanzek', 'hanžek', 'lešinari', 'lesinari', 
+                     'ološ', 'olos', 'papcino', 'papak', 'papčino', 'bosanko', 'bosanđeros', 'bosanceros',
+                     'bosancina', 'hercegovance', 'šupak', 'šupci', 'supak', 'supci', 'bosančeros', 'kuja', 
+                     'kujica', 'dementra', 'dementna', 'nakaza', 'katolibani', 'talibani', 'papčina', 'kuraba',
+                     'ganci', 'ljadro', 'retard', 'paksu', 'droca', 'express', 'srba', 'srbi', 'expres' ,
+                     'šuft', 'suft', 'ćifut', 'katoliban', 'kolje', 'klati', 'kolj', 'jambrusic', 'jambrušić',
+                     'tolusic', 'tolušić', 'siptar', 'balija', 'droca', 'acab', 'a.c.a.b.', 'radman', 
+                     'selekcija', 'sjajna zvijezdo', 'sjajna zvjezdo', 'celofanka', 'kravo', 'kobila', 
+                     'samoprozvani doktor za ljubav', 'drkolinda', 'poturica', 'poturico', 'isprdak']
+
+# ' fasist',
+#  ' fasista',
+#  ' fasiste',
+#  ' fasisti',
+#  ' fasizam',
+#  ' fasizma',
+#  ' fasizmu',
+
+
+# admin
+# admin01
+# admin24
+# admin_1
+# admin_24sata
+# admin_7
+# admina
+# adminama
+# admincek
+# admincic
+# admincice
+# admincicu
+# admincino
+# admine
+# adminee
+# admineee
+# admineeee
+# admineeeee
+# admineeeeeee
+# admineeeeeeee
+# admineeeeeeeee
+# admineeeeeeeeeeee
+# admineeeeeeeeeeeeeeeeeeeeee
+# adminejo
+# adminhe
+# admini
+# adminica
+
+# adolf
+# adolfa
+# adolfe
+# adolfeee
+# adolfeeeee
+# adolfima
+# adolfinjo
+# adolfland
+# adolfom
+# adolfov
+# adolfova
+# adolfu
+
+# 24casa
+# 24casova
+# 24cetnicki
+# 24cetnickoga
+# 24dobilo
+# 24glupost
+# 24guzice
+# 24h
+# 24kurac
+# 24kurca
+# 24kurira
+# 24pisamponjimasata
+# 24politka
+# 24pošto
+# 24s4ta
+# 24sat
+# 24sata
+# 24satajebovampasmater
+# 24satara
+# 24satni
+# 24satno
+# 24satnog
+# 24smrada
+# 24srbija
+# 24sta
+# 24trollsata
+# 24ur
+# 24ura
+# 24ure
+# 24v
+# 24vsata
+# 24yure
+# 24zonu
+# 24časa
+# 24časnom
+# 24časova
+# 24časovci
+# 24časovni
+# 24časta
+# 24čuke
+# 25sata
+
+
+word_to_rule = {'24sata':1}
+def check_blocked_words(text):
+    # Simply based on the texonomy 
+    rule = -1
+    conf = -1
+    rule_flag = False
+
+    for word in not_allowed_words:
+        match = re.search(word,text)
+        if match:
+            rule_flag = True
+            found_word = word #TODO: This misses if multiple words are present
+            break
+    
+    if rule_flag:
+        try:
+            rule = word_to_rule[found_word]
+        except:
+            rule = 3
+        conf = 0.9
+    
+    return rule, conf
+
+
+def check_rule_seven(text):
+    # To check based on the language and upper case
+    rule = -1
+    conf = -1
+    rule_flag = False
+    if text.isupper():
+        rule_flag = True
+    else:
+        text = " ".join(re.findall("[a-zA-Z]+", text))
+        words = text.split(' ')
+        c_w = len(words)
+        if c_w > 3:
+            # Check in two instances 
+            val1 = " ".join(words[:int(c_w/2)])
+            val2 = " ".join(words[int(c_w/2):])
+            for val in [val1, val2,value]:
+                try:
+                    lang2 = detect(val) #Easy to run 
+                except:
+                    lang2 = 'hr'
+                if lang2 not in valid_langs:
+                    try:
+                        lang1 = str(detector.detect_language_of(val))[9:]
+                    except:
+                        lang1 = 'CROATIAN'
+
+                    if lang1 in valid_langs:
+                       rule_flag = True
+                       break
+                else:
+                    rule_flag = True
+                    break
+    if rule_flag:
+        rule = 7
+        conf = 0.9
+    return rule, conf
+
 def compute_metrics(eval_preds):
 
     metric_acc = load_metric("accuracy")
