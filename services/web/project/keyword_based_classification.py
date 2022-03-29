6f32629d7ba511c3aebcb332f765c6801b2b445b
Ravi Shekhar <shekharRavi@users.noreply.github.com>
2022-03-29 10:44:55 +0100

    d

--- services/web/project/keyword_based_classification.py
+++ services/web/project/keyword_based_classification.py
@@ -62,7 +62,7 @@ def check_blocked_words(text,not_allowed_words=[]):
             rule_flag = True
             # found_word = word  # TODO: This misses if multiple words are present
             c+=1
-            if c > 1:
+            if c > 2:
                 break
     return rule_flag
 
