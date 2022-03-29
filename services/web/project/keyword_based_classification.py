c40b55e675d542078239e028257d2b8af215a554
Ravi Shekhar <shekharRavi@users.noreply.github.com>
2022-03-29 11:02:01 +0100

    d

--- services/web/project/keyword_based_classification.py
+++ services/web/project/keyword_based_classification.py
@@ -159,7 +159,7 @@ def keyword_based_classification(text):
             #Check based on keywords for Rule 2,3,4,5,6
             
             rule_words = [R2, R3,R4,R5,R6, all_words]
-            thresholds = [2, 2, 2, 2, 2, 2]  #For Major rule higher threshold
+            thresholds = [2, 2, 3, 2, 2, 2]  #For Major rule higher threshold
             rules = [2,3,4,5,6,8]
             for rule_word, threshold, rule_key in zip(rule_words,thresholds, rules):
                 rule_flag = keyword_to_rule(text,rule_word,threshold=threshold)
